4af45cb0acb3ecadbcee2a3a3a4c23fce5a28957
sebtn <tonomudo@gmail.com>
2015-12-23 23:28:31 +0000

    Intial commmit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PreAgile::Application.config.secret_key_base = '52b1ffff5bf5fb19d8fc5164781dd33c04366dcbfee32a8d51bcab1c0e98110c60ca499cf68202e437b1ea007f9b1753109b3fca43029572bb0ff836d6d12c8e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PreAgile::Application.config.session_store :cookie_store, key: '_pre-agile_session'
